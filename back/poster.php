474ad15d85d842a4ce08d09ae3b792d7fdb7c713
HelloCodeman <955khm@gmail.com>
2024-01-11 15:26:18 +0800

    完成預告片的排序按鈕功能

--- back/poster.php
+++ back/poster.php
@@ -79,3 +79,18 @@
     </div>
   </form>
 </div>
+
+<script>
+  $('when').on(click, function() {
+    let id = $(this).data('id');
+    let sw = $(this).data('sw');
+    let table = 'poster';
+    $.post("./api/sw.php", {
+      id,
+      sw,
+      table
+    }, () => {
+      location.reload()
+    })
+  })
+</script>
